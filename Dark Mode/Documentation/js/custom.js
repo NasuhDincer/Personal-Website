33c32af3fc48c9529610e7632220172f44ae743b
Nasuh Dincer <63503948+NasuhDincer@users.noreply.github.com>
2022-03-13 23:09:39 +0300

    Add files via upload

---
+++ Dark Mode/Documentation/js/custom.js
@@ -0,0 +1,14 @@
+/**
+ * Author:    Nasuh Dincer
+ * Created:   13/03/2022
+ *
+ * (c) Copyright by Nasuh Dincer Corp.
+ */
+
+$(function() {
+"use strict";
+    
+    $('#nav').singlePageNav();
+    
+
+});
